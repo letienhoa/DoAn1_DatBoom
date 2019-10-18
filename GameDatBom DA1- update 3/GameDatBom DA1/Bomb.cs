ac8866c0f2855742f724dcc49c36dc4dee2187de
Vuonglestudent <55451678+Vuonglestudent@users.noreply.github.com>
2019-10-18 13:06:13 +0700

    Add files via upload

---
+++ GameDatBom DA1- update 3/GameDatBom DA1/Bomb.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace GameDatBom_DA1
+{
+    public class Bomb
+    {
+        public enum BombImage
+        {
+            bomb,
+            bombbang
+        }
+        System.Timers.Timer ThoiGianBomb = new System.Timers.Timer(500);
+        public Control btnBomb { get; set; }
+        private BombImage ImageBomb { get; set; }
+
+        private DateTime ThoiGian { get; set; }
+
+        public event EventHandler MatBomb;
+
+        public Bomb(Control F1btnBomb,BombImage F1ImageBomb)
+        {
+            this.btnBomb = F1btnBomb;
+            ThoiGianBomb.Elapsed += ThoiGianBomb_Elapsed;
+            this.ImageBomb = F1ImageBomb;
+            this.ThoiGianBomb.Enabled = true;
+            this.ThoiGian = DateTime.Now.AddSeconds(3);
+        }
+
+        private void ThoiGianBomb_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            switch(ImageBomb)
+            {
+                case BombImage.bomb:
+                    if (DateTime.Now > ThoiGian)
+                    {
+                        BombNo();
+                    }
+                    break;
+                case BombImage.bombbang:
+                    Point a = btnBomb.Location;
+                    
+                    
+                    break;
+            }
+            
+        }
+
+        private void BombNo()
+        {
+            this.ThoiGianBomb.Enabled = false;
+            if(this.MatBomb!=null)
+            {
+                this.btnBomb.BackgroundImage = Properties.Resources.nen1;
+                this.ThoiGianBomb.Dispose();
+                this.MatBomb(this, EventArgs.Empty);
+            }
+        }
+
+
+        // Form1: Dispose
+        internal void Dispose()
+        {
+            
+            this.ThoiGianBomb.Dispose();
+            this.btnBomb.BackgroundImage = Properties.Resources.nen1;
+        }
+    }
+}
--- GameDatBom DA1- update 3/GameDatBom DA1/Form1.cs
+++ GameDatBom DA1- update 3/GameDatBom DA1/Form1.cs
@@ -22,7 +22,7 @@ namespace GameDatBom_DA1
         bool lentren;
         bool xuongduoi;
         bool datbom;
-    
+        List<Bomb> lstBombs = new List<Bomb>();
         int tocdochoi = 35;
 
 
@@ -41,66 +41,119 @@ namespace GameDatBom_DA1
 
         private void Anxuong(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
+            switch (e.KeyCode)
             {
-                sangtrai = true;
-                player.Image = Properties.Resources.trai;
-                player.BackgroundImageLayout = ImageLayout.Stretch;
-                player.BackgroundImage = Properties.Resources.nen1;
-            }
-            if (e.KeyCode == Keys.Right)
-            {
-                sangphai = true;
-                player.Image = Properties.Resources.phai;
-                player.BackgroundImageLayout = ImageLayout.Stretch;
-                player.BackgroundImage = Properties.Resources.nen1;
-            }
-            if (e.KeyCode == Keys.Up)
-            {
-                lentren = true;
-                player.Image = Properties.Resources.sau;
-                player.BackgroundImageLayout = ImageLayout.Stretch;
-                player.BackgroundImage = Properties.Resources.nen1;
-            }
-            if (e.KeyCode == Keys.Down)
-            {
-                xuongduoi = true;
-                player.Image = Properties.Resources.truoc2;
-                player.BackgroundImageLayout = ImageLayout.Stretch;
-                player.BackgroundImage = Properties.Resources.nen1;
-            }
-            if (e.KeyCode == Keys.Enter)
-            {
-                datbom = true;
-                Point h = player.Location;
-                xulybom(h);
+                case Keys.Left:
+                    sangtrai = true;
+                    player.Image = Properties.Resources.trai;
+                    player.BackgroundImageLayout = ImageLayout.Stretch;
+                    player.BackgroundImage = Properties.Resources.nen1;
+                    break;
+                case Keys.Right:
+                    sangphai = true;
+                    player.Image = Properties.Resources.phai;
+                    player.BackgroundImageLayout = ImageLayout.Stretch;
+                    player.BackgroundImage = Properties.Resources.nen1;
+                    break;
+                case Keys.Up:
+                    lentren = true;
+                    player.Image = Properties.Resources.sau;
+                    player.BackgroundImageLayout = ImageLayout.Stretch;
+                    player.BackgroundImage = Properties.Resources.nen1;
+                    break;
+                case Keys.Down:
+                    xuongduoi = true;
+                    player.Image = Properties.Resources.truoc2;
+                    player.BackgroundImageLayout = ImageLayout.Stretch;
+                    player.BackgroundImage = Properties.Resources.nen1;
+                    break;
+                case Keys.Space:
+                    datbom = true;
+                    //Point h = player.Location;
+                    xulybom(player.Location);
+                    
+                    break;
 
             }
+            //if (e.KeyCode == Keys.Left)
+            //{
+            //    sangtrai = true;
+            //    player.Image = Properties.Resources.trai;
+            //    player.BackgroundImageLayout = ImageLayout.Stretch;
+            //    player.BackgroundImage = Properties.Resources.nen1;
+            //}
+            //if (e.KeyCode == Keys.Right)
+            //{
+            //    sangphai = true;
+            //    player.Image = Properties.Resources.phai;
+            //    player.BackgroundImageLayout = ImageLayout.Stretch;
+            //    player.BackgroundImage = Properties.Resources.nen1;
+            //}
+            //if (e.KeyCode == Keys.Up)
+            //{
+            //    lentren = true;
+            //    player.Image = Properties.Resources.sau;
+            //    player.BackgroundImageLayout = ImageLayout.Stretch;
+            //    player.BackgroundImage = Properties.Resources.nen1;
+            //}
+            //if (e.KeyCode == Keys.Down)
+            //{
+            //    xuongduoi = true;
+            //    player.Image = Properties.Resources.truoc2;
+            //    player.BackgroundImageLayout = ImageLayout.Stretch;
+            //    player.BackgroundImage = Properties.Resources.nen1;
+            //}
+            //if (e.KeyCode == Keys.Enter)
+            //{
+            //    datbom = true;
+            //    Point h = player.Location;
+            //    xulybom(h);
+
+            //}
 
         }
 
         private void Anlen(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
+            switch(e.KeyCode)
             {
-                sangtrai = false;
+                case Keys.Left:
+                    sangtrai = false;
+                    break;
+                case Keys.Right:
+                    sangphai = false;
+                    break;
+                case Keys.Up:
+                    lentren = false;
+                    break;
+                case Keys.Down:
+                    xuongduoi = false;
+                    break;
+                case Keys.Space:
+                    datbom = false;
+                    break;
+
             }
-            if (e.KeyCode == Keys.Right)
-            {
-                sangphai = false;
-            }
-            if (e.KeyCode == Keys.Up)
-            {
-                lentren = false;
-            }
-            if (e.KeyCode == Keys.Down)
-            {
-                xuongduoi = false;
-            }
-            if (e.KeyCode == Keys.Enter)
-            {
-                datbom = false;
-            }
+            //if (e.KeyCode == Keys.Left)
+            //{
+            //    sangtrai = false;
+            //}
+            //if (e.KeyCode == Keys.Right)
+            //{
+            //    sangphai = false;
+            //}
+            //if (e.KeyCode == Keys.Up)
+            //{
+            //    lentren = false;
+            //}
+            //if (e.KeyCode == Keys.Down)
+            //{
+            //    xuongduoi = false;
+            //}
+            //if (e.KeyCode == Keys.Enter)
+            //{
+            //    datbom = false;
+            //}
         }
         
         private void Timer1_Tick(object sender, EventArgs e)
@@ -190,19 +243,42 @@ namespace GameDatBom_DA1
             goc4.X = player.Location.X;
             goc4.Y = player.Location.Y+player.Height;
         }
-        private void xulybom(Point a)
+        private void xulybom(Point player)
         {
-            foreach(Control x in this.panel1.Controls)
+            foreach(Control bomb in this.panel1.Controls)
             {
-                if(x is Button && x.Location.X==a.X&&x.Location.Y==a.Y)
+                if(bomb is Button && bomb.Location.X== player.X && bomb.Location.Y== player.Y)
                 {
-                    int goc1x = (player.Location.X) / 35;
-                    int goc1y = (player.Location.Y) / 35;
-                    x.BackgroundImage= Properties.Resources.bomb;
-                    x.BackgroundImageLayout = ImageLayout.Stretch;
+                    int goc1x = (player.X) / 35;
+                    int goc1y = (player.Y) / 35;
+                    bomb.BackgroundImage= Properties.Resources.bomb;
+                    bomb.BackgroundImageLayout = ImageLayout.Stretch;
                     LoaiMap.map1[goc1y,goc1x] = 1;
+                    Bomb btnBomb = new Bomb(bomb, Bomb.BombImage.bomb);
+                    btnBomb.MatBomb += Mat_btnBomb;
+                    this.lstBombs.Add(btnBomb);
+                    
                 }
             }           
+        }
+
+        private void Mat_btnBomb(object sender, EventArgs e)
+        {
+            Bomb objBomb = sender as Bomb;
+            if (objBomb != null)
+            {
+                lstBombs.Remove(objBomb);
+                Point a = objBomb.btnBomb.Location;
+                LoaiMap.map1[a.Y/35, a.X/35] = 0;
+
+                foreach (Control bombNo in this.panel1.Controls)
+                {
+                    Bomb bombLan = new Bomb(bombNo, Bomb.BombImage.bombbang);
+                }
+
+                objBomb.Dispose();
+                
+            }
         }
     }
 }
