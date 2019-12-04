0bc8080fb74ff6a41ed7170bea10218823d13e5e
Vuonglestudent <55451678+Vuonglestudent@users.noreply.github.com>
2019-12-04 16:53:04 +0700

    vuong

--- GameDatBom DA1- update 3/GameDatBom DA1/taogiaodien.cs
+++ GameDatBom DA1- update 3/GameDatBom DA1/taogiaodien.cs
@@ -18,8 +18,8 @@ namespace GameDatBom_DA1
         {
             this.khungbando = pnl1;
         }
-        public static List<List<Button>> mangluu = new List<List<Button>>();
-        public static List<int> listdiem = new List<int>();
+        public static List<List<Button>> mangluu = new List<List<Button>>(); //Mảng 2 chiều
+        public static List<int> listdiem = new List<int>(); 
 
         public void taomatran()
         {
